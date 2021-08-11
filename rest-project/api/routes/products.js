24be0013f288e8623b550bfe614ae545a3c92889
Jason Zhang <jasonzhang3178@gmail.com>
2021-08-11 13:21:08 -0700

    mvc

--- rest-project/api/routes/products.js
+++ rest-project/api/routes/products.js
@@ -1,113 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('../models/product');
-const mongoose = require('mongoose');
-router.get('/', (req, res, next) => {
-    Product.find()
-    .select('name price _id')
-    .exec()
-    .then(doc => {
-        const response = {
-            count: doc.length,
-            products: doc.map(doc => {
-                return{
-                    name: doc.name,
-                    price: doc.price,
-                    _id: doc._id,
-                    request: {
-                        type: 'GET',
-                        url: 'http://localhost:3000/products' + doc._id
-                    }
-                }
-            })
-        };
-        //if (doc.length >= 0) {
-        res.status(200).json(response);
-        /*} else {
-            res.status(404).json({
-                message: "no entries used"
-            })
-        }*/
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    });
-});
+const authcheck = require('../middleware/auth-check');
+const productsController = require("../controllers/products");
 
-router.post('/', (req, res, next) => {
-    const product = new Product({
-        _id: new mongoose.Types.ObjectId(),
-        name: req.body.name,
-        price: req.body.price
-    });
-    product
-    .save()
-    .then(result => {
-        console.log(result);
-        res.status(201).json({
-            message: "Successful creation of product",
-            createdProduct: {
-                name: result.name,
-                price: result.price,
-                _id: result._id,
-                request: {
-                    type: 'POST',
-                    url: 'http://localhost:3000/products/' + result._id
-                }
-            }
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error:err
-        });
-    });
-
-    });
-});
+router.get('/', productsController.products_get_all);
+router.post('/', authcheck, productsController.products_create);
 //add more descriptive api stuff
-router.get('/:productId', (req, res, next) => {
-    const id = req.params.productId;
-    Product.findById(id).exec().then(doc => {
-        console.log("From database", doc);
-        if (doc) {
-            res.status(200).json(doc);
-        } else {
-            res.status(404).json({message: "not valid id"});
-        }
-        //res.status(200).json({doc})
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json({error: err});
-    });
-});
-
-router.patch('/:productId', (req, res, next) => {
-    const id = req.params.productId;
-    const updateOps = {};
-    for (const ops of req.body) {
-        updateOps[ops.propName] = ops.value;
-    }
-    Product.findByIdAndUpdate(id, { $set: updateOps }, { new: true})
-    .then(result => res.status(200).json(result))
-    .catch(err => res.status(500).json({ error: err}))
-});
-
-router.delete('/:productId', (req, res, next) => {
-    const id = req.params.productId;
-    Product.remove({_id: id})
-    .exec()
-    .then(result => {
-        res.status(200).json(result);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        })
-    })
-});
+router.get('/:productId', authcheck, productsController.products_get);
+router.patch('/:productId', authcheck, productsController.products_patch);
+router.delete('/:productId', authcheck, productsController.products_delete);
 module.exports = router;
